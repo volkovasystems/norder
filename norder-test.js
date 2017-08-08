8e2ec97f2839216149785741de29644f746104f7
johnlenon <johnlenonmaghanoy@gmail.com>
2017-08-08 18:53:28 +0800

    Updated test.

--- norder-test.js
+++ norder-test.js
@@ -6,24 +6,24 @@ assert.equal( norder( {
 	"apple": 0,
 	"cherry": 1,
 	"orange": 2
-} ), true, "should be true" );
+} ), true, "should be equal to true" );
 
 assert.equal( norder( {
 	"apple": 0,
 	"cherry": 1,
 	"orange": 5
-} ), false, "should be false" );
+} ), false, "should be equal to false" );
 
 assert.equal( norder( {
 	"apple": 0,
 	"cherry": 1,
 	"orange": 3
-} ), false, "should be false" );
+} ), false, "should be equal to false" );
 
 assert.equal( norder( {
 	"apple": 0,
 	"cherry": 1,
 	"orange": 1
-} ), false, "should be false" );
+} ), false, "should be equal to false" );
 
 console.log( "ok" );
